7e911011c5b17d75c45633090a3a93283d1b2772
asanagustin-godaddy <asanagustin@godaddy.com>
2024-02-21 15:41:00 -0700

    add base script with react-web-ar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hiro from "./Hiro";
+import TrainTicket from "./TrainTicket";
+import Railcard from "./Railcard";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TrainTicket />
+      <div>hello</div>
+    </div>
+  );
+}

5cddff9216060782191134e1cd72021e92a3a9e5
asanagustin-godaddy <asanagustin@godaddy.com>
2024-02-22 12:15:53 -0700

    add speech to text and robot gltf (in progress)

---
+++ src/Speech.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
+
+// ref: https://codesandbox.io/p/github/Ronald-Cifuentes/react-speech-to-text/master
+export default function Speech() {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false
+  });
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+  return (
+    <div>
+      <p>Recording: {isRecording.toString()}</p>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {/* {interimResult && <li>{interimResult}</li>} */}
+      </ul>
+    </div>
+  );
+}
